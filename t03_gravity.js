a236519c16e674b5d058e30b17d255a1fece5504
reas-dtech <24582rd@hvhs.school.nz>
2026-02-09 10:58:18 +1300

    got up to world.gravity.y in the instructions

--- t03_gravity.js
+++ t03_gravity.js
@@ -1,7 +1,7 @@
 /*******************************************************/
 // P5.play: t03_gravity
-// Sprite falls due to gravity
-// Written by ???
+// sprite falls due to gravity
+/// Written by ???
 /*******************************************************/
 	
 /*******************************************************/
@@ -9,6 +9,15 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+cnv = new Canvas(windowWidth ,  windowHeight);
+world.gravity.y = 10;
+
+box = new Sprite(0, 0, 100, 100, '100');
+circle = new Sprite(100, 100, 50);
+circle.collider = 'circle';
+spin = new Sprite(300, -200, 250, 300, '300')
+spin.rotationSpeed = -10;
+spin.vel.y = 1;
 
 }
 	
@@ -16,7 +25,9 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	
+	spin.color =' #87CEEB';
+	box.color ='#2E8B57';
+	background('#000000');
 }
 
 /*******************************************************/
